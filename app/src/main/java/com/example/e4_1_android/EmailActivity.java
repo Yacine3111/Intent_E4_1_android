bffb455763cf577d4f47bb74b44ea8d228f41fb1
YACINE BOUREZAK <2288656@cegepgranby.qc.ca>
2025-02-24 23:26:39 -0500

    ReceiveMailActivity.java recieve the email sent

--- app/src/main/java/com/example/e4_1_android/EmailActivity.java
+++ app/src/main/java/com/example/e4_1_android/EmailActivity.java
@@ -2,12 +2,15 @@ package com.example.e4_1_android;
 
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -17,7 +20,6 @@ public class EmailActivity extends AppCompatActivity {
     Intent emailIntent;
     Button button;
     EditText editTextEmail,editTextTitle,editTextMSG;
-    Uri uri;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,7 +30,7 @@ public class EmailActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        emailIntent=new Intent(Intent.ACTION_SENDTO);
+
         button=findViewById(R.id.button_email);
 
         editTextEmail=findViewById(R.id.editTextEmailAddress);
@@ -36,19 +38,17 @@ public class EmailActivity extends AppCompatActivity {
         editTextMSG=findViewById(R.id.editTextBody);
 
         button.setOnClickListener(v->{
+            emailIntent=new Intent(Intent.ACTION_SENDTO,Uri.parse("mailto:"+editTextEmail.getText().toString()));
             String subjectSrt=editTextTitle.getText().toString();
             String bodySrt=editTextMSG.getText().toString();
 
-            uri=Uri.parse("mailto:"+editTextEmail.getText().toString());
-
-            emailIntent.setData(uri);
 
             //marche pas
             emailIntent.putExtra(Intent.EXTRA_SUBJECT,subjectSrt);
             emailIntent.putExtra(Intent.EXTRA_TEXT,bodySrt);
 
             try {
-                startActivity(emailIntent);
+                startActivity(Intent.createChooser(emailIntent,"Send mail..."));
             } catch (ActivityNotFoundException e) {
                 MainActivity.errorMessage(this);
             }
--- app/src/main/java/com/example/e4_1_android/ReceiveMailActivity.java
+++ app/src/main/java/com/example/e4_1_android/ReceiveMailActivity.java
@@ -1,4 +1,46 @@
 package com.example.e4_1_android;
 
-public class ReceiveMailActivity {
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ReceiveMailActivity extends AppCompatActivity {
+    TextView textView;
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_email);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.email_layout), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        textView=findViewById(R.id.textView3);
+        button=findViewById(R.id.button_email);
+
+        textView.setText("Recevoir un email à envoyer");
+        button.setEnabled(false);
+
+        Intent intent=getIntent();
+        Uri uri=intent.getData();
+
+        String email = uri.toString();
+
+        EditText editText=findViewById(R.id.editTextEmailAddress);
+        editText.setText(email);
+
+
+    }
 }
